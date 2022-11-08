16c1da911fbca807697aa9753673b9ade76f4d6d
yaoqijun <yaoqijunmail@foxmail.com>
2022-11-08 18:46:30 +0800

    add all

--- server/server.go
+++ server/server.go
@@ -28,10 +28,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	opt := grpc.ChainUnaryInterceptor(firstServerInterceptor)
+	s := grpc.NewServer(opt)
 	lib.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func firstServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	log.Printf("服务端一元过滤器1：开始")
+	res, err := handler(ctx, req)
+	log.Printf("服务端一元过滤器1：完成")
+	return res, err
 }
